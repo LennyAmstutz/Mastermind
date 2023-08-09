34078fab33813b34be4e9fccc73962072f34afd9
LennyAmstutz <lennyamstutz@gmail.com>
2023-08-09 09:41:49 +0200

    add array

--- src/ch.css.mastermind/Main.java
+++ src/ch.css.mastermind/Main.java
@@ -1,9 +1,25 @@
 package ch.css.mastermind;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+
+        String[] color = {"Red", "Blue", "Green", "Yellow", "Pink", "White", "Black", "Orange"};
+
+        for (int i = 0; i < 8; i++){
+
+            System.out.println(color[i]);
+        }
+
+
+
+
+
 
     }
 }
