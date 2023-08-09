255ec0293cbcc398ea9d111cc2922202065b503f
LennyAmstutz <lennyamstutz@gmail.com>
2023-08-09 13:38:53 +0200

    Beide teile wurden zusammen gefügt

--- src/ch.css.mastermind/Main.java
+++ src/ch.css.mastermind/Main.java
@@ -2,11 +2,49 @@ package ch.css.mastermind;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
+
+        boolean start = false;
+        boolean beginn = false;
+
+        if (beginn == false){
+
+
+        while (!beginn){
+            System.out.println(" Drücke I für Informationen und S für Start");
+
+            Scanner sc = new Scanner(System.in);
+            String key = sc.next();
+            if (key.equals("I") || key.equals("i")) {
+                System.out.println("""
+                    Das Ziel des Spieles ist die Farbkombination, die der Computer sich überlegt hat in 12 Runden heraus zu finden.
+                    Es gibt die Farben Grün, Blau, Rot, Gelb, Orange, Pink, Schwarz und Weiss.
+                    Der Computer überlegt sich aus diesen Farben eine Kombination, diese Kombination ist vier Farben lang.
+                    Es kann aber auch sein, dass eine Farbe zwei mal vorhanden ist. Es müssen die Farben stimmen und auch die Position der Farben.
+                     Der Computer sagt dir jede Runde wie viele Farben das richtig sind und wie viele Positionen das richtig sind
+                    """);
+                System.out.println("Drücke S für Start");
+                key = sc.next();
+                if (key.equals("S") || key.equals("s")) {
+                    start = true;
+                    beginn = true;
+                    System.out.println(start);
+                }
+            }else if (key.equals("S") || key.equals("s")) {
+                start = true;
+                beginn = true;
+                System.out.println(start);
+            }else
+                System.out.println("Diese Taste hat keine Funktion");
+        }
+
+        }
+
 
         for (int i = 0; i < 4; i++) {
 
