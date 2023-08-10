5565113c3a0811cee874f0955e4f7728d48f774a
LennyAmstutz <lennyamstutz@gmail.com>
2023-08-10 10:14:36 +0200

    Runden bis 12

--- src/ch.css.mastermind/Main.java
+++ src/ch.css.mastermind/Main.java
@@ -1,6 +1,5 @@
 package ch.css.mastermind;
 
-
 import java.util.Scanner;
 
 
@@ -10,45 +9,39 @@ public class Main {
     public static void main(String[] args) {
 
 
-        //int age = 27;
-
-        //int age2;
-        //age2 = 29;
-        //System.out.println(age);
-
         boolean start = false;
         boolean beginn = false;
 
-        if (beginn == false){
+        if (beginn == false) {
 
 
-        while (!beginn){
-            System.out.println(" Drücke I für Informationen und S für Start");
+            while (!beginn) {
+                System.out.println(" Drücke I für Informationen und S für Start");
 
-            Scanner sc = new Scanner(System.in);
-            String key = sc.next();
-            if (key.equals("I") || key.equals("i")) {
-                System.out.println("""
-                    Das Ziel des Spieles ist die Farbkombination, die der Computer sich überlegt hat in 12 Runden heraus zu finden.
-                    Es gibt die Farben Grün, Blau, Rot, Gelb, Orange, Pink, Schwarz und Weiss.
-                    Der Computer überlegt sich aus diesen Farben eine Kombination, diese Kombination ist vier Farben lang.
-                    Es kann aber auch sein, dass eine Farbe zwei mal vorhanden ist. Es müssen die Farben stimmen und auch die Position der Farben.
-                     Der Computer sagt dir jede Runde wie viele Farben das richtig sind und wie viele Positionen das richtig sind
-                    """);
-                System.out.println("Drücke S für Start");
-                key = sc.next();
-                if (key.equals("S") || key.equals("s")) {
+                Scanner sc = new Scanner(System.in);
+                String key = sc.next();
+                if (key.equals("I") || key.equals("i")) {
+                    System.out.println("""
+                            Das Ziel des Spieles ist die Farbkombination, die der Computer sich überlegt hat in 12 Runden heraus zu finden.
+                            Es gibt die Farben Grün, Blau, Rot, Gelb, Orange, Pink, Schwarz und Weiss.
+                            Der Computer überlegt sich aus diesen Farben eine Kombination, diese Kombination ist vier Farben lang.
+                            Es kann aber auch sein, dass eine Farbe zwei mal vorhanden ist. Es müssen die Farben stimmen und auch die Position der Farben.
+                             Der Computer sagt dir jede Runde wie viele Farben das richtig sind und wie viele Positionen das richtig sind
+                            """);
+                    System.out.println("Drücke S für Start");
+                    key = sc.next();
+                    if (key.equals("S") || key.equals("s")) {
+                        start = true;
+                        beginn = true;
+                        System.out.println(start);
+                    }
+                } else if (key.equals("S") || key.equals("s")) {
                     start = true;
                     beginn = true;
-                    System.out.println(start);
-                }
-            }else if (key.equals("S") || key.equals("s")) {
-                start = true;
-                beginn = true;
 
-            }else
-                System.out.println("Diese Taste hat keine Funktion");
-        }
+                } else
+                    System.out.println("Diese Taste hat keine Funktion");
+            }
 
         }
 
@@ -61,30 +54,27 @@ public class Main {
             int myMax = 7;
 
 
-            int random_int1 = (int)Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+            int random_int1 = (int) Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
 
 
             System.out.println(color[random_int1]);
 
-            }
-
-
-        Scanner fr = new Scanner(System.in);
-        System.out.println("Farbe:");
-
-        String userName = fr.nextLine();
-
-
-            if (){
 
 
         }
+        for (int n = 1; n <= 12; n++) {
 
 
+            for (double d = 0; d < 4; d++) {
+
+                Scanner fr = new Scanner(System.in);
+                System.out.println("Farbe:");
+
+                String key3 = fr.nextLine();
+
+                System.out.println("Runde" + n );
+
+            }
+        }
     }
-
-
 }
-
-
-
